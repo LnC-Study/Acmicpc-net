4b00e188ff2d8fa21e721fce41f77bdf0df7c9f6
kdhong <kdhong@naver.com>
2019-01-14 15:23:12 +0900

    add_6549

---
+++ Divide&Conquer/6549 히스토그램에서 가장 큰 직사각형/README.md
@@ -0,0 +1,1 @@
+# [6549 히스토그램에서 가장 큰 직사각형](https://www.acmicpc.net/problem/6549)
---
+++ Divide&Conquer/6549 히스토그램에서 가장 큰 직사각형/ckw_cpp.cpp
@@ -0,0 +1,82 @@
+#define _CRT_SECURE_NO_WARNINGS
+#include <stdio.h>
+#include <vector>
+#include <algorithm>
+
+using namespace std;
+
+vector<int> histogram;
+
+long long max_rectangle(int start, int end) {
+
+	if (start == end)
+		return histogram[start];
+
+	long long ret = 0;
+
+	int mid = (start + end) / 2;
+	ret = max(ret, max_rectangle(start, mid));
+	ret = max(ret, max_rectangle(mid + 1, end));
+
+	int left = mid;
+	int right = mid + 1;
+
+	int height = min(histogram[left], histogram[right]);
+
+	long long area = height * (right - left + 1);
+	ret = max(ret, area);
+
+	for (; !(left == start && right == end) ;) {
+		
+		if (left > start && right < end) {
+			histogram[left - 1] > histogram[right + 1] ? left-- : right++;
+			height = min(height, histogram[left]);
+			height = min(height, histogram[right]);
+		}
+		else if (left > start) {
+			left--;
+			height = min(height, histogram[left]);
+		}
+		else {
+			right++;
+			height = min(height, histogram[right]);
+		}
+
+		area = height * (right - left + 1);
+		ret = max(ret, area);
+
+	}
+
+	return ret;
+}
+
+void data_in() {
+
+	freopen("input.txt", "r", stdin);
+
+	while (true) {
+		int N;
+		scanf("%d ", &N);
+
+		if (N == 0)
+			break;
+
+		histogram = vector<int>(N);
+
+		for (int i = 0; i < N; i++) {
+			scanf("%d ", &histogram[i]);
+		}
+
+		int result = max_rectangle(0, N - 1);
+		printf("%lli\n", result);
+
+		histogram.erase(histogram.begin(), histogram.end());
+	}
+		
+}
+
+int main() {
+
+	data_in();
+
+}
