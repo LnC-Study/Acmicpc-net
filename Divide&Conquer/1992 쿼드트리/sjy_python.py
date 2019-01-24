6b8ec14874132f232f99ab0f5c50b549c5e44c19
suy427 <sondahum@gmail.com>
2019-01-24 18:31:42 +0900

    add sjy_python.py of Problem #1992

---
+++ Divide&Conquer/1992 쿼드트리/sjy_python.py
@@ -0,0 +1,25 @@
+length = int(input())
+
+square = []
+
+for cnt in range(length):
+    square.append(list(input()))
+
+
+# def compress(square):
+    # if square <= 1: # 한칸짜리면
+    #     return its value    그거 그대로 출력
+    #
+    # else: # 2칸짜리 이상이면
+    #     return compress(square/2) # 반 나눠서 recursive
+
+
+# 8
+# 11110000
+# 11110000
+# 00011100
+# 00011100
+# 11110000
+# 11110000
+# 11110011
+# 11110011
