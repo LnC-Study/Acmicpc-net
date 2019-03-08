c305541e4f669811543b7949bcd72caa9179cd28
Shindd <nidor02@hotmail.com>
2019-03-08 17:24:11 +0900

    Add files via upload

---
+++ __Samsum swexpert/15684 사다리 조작/sdk_python.py
@@ -0,0 +1,10 @@
+def data_in():
+    N, M, H = map( int, input().split())
+    return N, M, H, [ list(map( int, input().split()))]
+
+def solution( vertical_lines, horizontal_lines, connected):
+    pass
+
+if __name__ == '__main__':
+    vertical_lines, horizontal_lines, connected = data_in()
+    solution( vertical_lines, horizontal_lines, connected)
