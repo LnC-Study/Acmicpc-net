f7b6e5f06d3f71c0687cc2237fb920038187ef5c
suy427 <sondahum@gmail.com>
2019-03-15 09:39:28 +0900

    corrected

--- __Samsung기출/15686 치킨 배달/sjy_python.py
+++ __Samsung기출/15686 치킨 배달/sjy_python.py
@@ -15,16 +15,17 @@ for row in range(N):
 
 
 def comb(total, toPick): # 전체 치킨집에서 몇개를 뽑는다 --> chicken 배열에서 M개를 뽑음(인덱스)
-    global city, house, chicken, chickenDistance
+    global house, chicken
     if toPick == 0:
-        tmp = [999 for _ in range(len(house))]
+        tmp = [99999 for _ in range(len(house))]
         for idx in picked:
             for h_idx in range(len(house)):
                 cur = chicken[idx] # idx번째 치킨집의 위치
                 val = abs(house[h_idx][0] - cur[0]) + abs(house[h_idx][1] - cur[1])
                 if tmp[h_idx] > val:
                     tmp[h_idx] = val
-            dis_each_home.append(tmp)
+
+        dis_each_home.append(tmp)
 
     if len(picked) == 0:
         smallest = 0
@@ -38,8 +39,7 @@ def comb(total, toPick): # 전체 치킨집에서 몇개를 뽑는다 --> chicke
 
 
 comb(len(chicken), M)
-
-min = 999
+min = 99999
 for each in dis_each_home:
     if sum(each) < min:
         min = sum(each)
