1f0a3f62e8fc8ee3a1320046209c80b855d9872c
suy427 <sondahum@gmail.com>
2019-04-04 15:17:03 +0900

    Jo-Ji-TTa~~

---
+++ __Samsung기출/14503 로봇청소기/sjy_python.py
@@ -0,0 +1,69 @@
+import collections
+
+N, M = list(map(int, input().split(' ')))
+rPos, cPos, initDir = list(map(int, input().split(' ')))
+rooms = [list(map(int, input().split(' '))) for _ in range(N)]
+direction = [(0, -1), (1, 0), (0, 1), (-1, 0)] # 차례대로 북,서,남,동 --> 0, 1, 2, 3 쪽에서의 왼쪽
+di = {0: 0, 1: 3, 2: 2, 3: 1} # 차례대로 북,동,남,서 --> 0, 1, 2, 3 쪽에서의 왼쪽
+
+
+class cleaner:
+    def __init__(self, row, col, dir, N, M):
+        self.row = row
+        self.col = col
+        self.dir = dir
+        self.visited = rooms
+        self.done = 0
+        self.queue = collections.deque([])
+        self.back = 1
+
+    def is_in(self, row, col):
+        if (0 <= row < M) and (0 <= col < N):
+            return True
+
+        return False
+
+    def go(self):
+        self.visited[self.row][self.col] = 1
+        self.queue.append((self.row, self.col))
+        self.done += 1
+
+        while len(self.queue) > 0:
+            cur = self.queue.popleft()
+
+            #self.back = 1
+            for next in range(4):
+                curDir = direction[self.dir] # 현재 방향에서 왼쪽이 curDir
+                nextRow, nextCol = cur[0] + curDir[0], cur[1] + curDir[1] # 현위치에서 바라보는 방향으로 왼쪽칸
+                if self.is_in(nextRow, nextCol) is True:
+                    if self.visited[nextRow][nextCol] == 0: # 이게 격자 안에 있고, 방문하지 않은거
+                        self.visited[nextRow][nextCol] = 1
+                        self.queue.append((nextRow, nextCol))
+                        self.done += 1
+                        self.back = 0
+                        break
+
+                self.dir += 1
+                if self.dir >= 4:
+                    self.dir = 0
+
+            if self.back == 1:
+                if self.dir == 0:
+                    back = (cur[0]+1, cur[1])
+                elif self.dir == 1:
+                    back = (cur[0], cur[1]+1)
+                elif self.dir == 2:
+                    back = (cur[0]-1, cur[1])
+                else:
+                    back = (cur[0], cur[1]-1)
+
+                self.queue.appendleft(back)
+                self.back += 1
+
+            elif self.back == 2:
+                return self.done
+
+
+robot = cleaner(rPos, cPos, di[initDir], N, M)
+print(robot.go())
+
