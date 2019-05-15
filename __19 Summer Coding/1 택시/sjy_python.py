f5f09dda66200906af19f83fc7c1221b2c0abb59
suy427 <sondahum@gmail.com>
2019-05-15 12:32:15 +0900

    push problem#1 sjy

--- __19 Summer Coding/1 택시/sjy_python.py
+++ __19 Summer Coding/1 택시/sjy_python.py
@@ -1,1 +1,29 @@
-iii
+# cheated sunny's..TT
+
+people = list(map(int, input().split(' ')))
+
+
+def sol(groups):
+    taxi = 0
+    head = 0
+    tail = len(groups)-1
+
+    while head <= tail:
+        sum = groups[head] + groups[tail]
+        if sum == 4:
+            head += 1
+            tail -= 1
+
+        elif sum < 4:
+            head += 1
+
+        else:
+            tail -= 1
+
+        taxi += 1
+
+    return taxi
+
+
+print(sol(sorted(people)))
+
