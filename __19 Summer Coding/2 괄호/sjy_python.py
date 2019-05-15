fc4c71df71db4cfb35980845b57bda20124d3132
suy427 <sondahum@gmail.com>
2019-05-15 11:49:13 +0900

    push problem#2 sjy

--- __19 Summer Coding/2 괄호/sjy_python.py
+++ __19 Summer Coding/2 괄호/sjy_python.py
@@ -1,1 +1,34 @@
-ii
+import itertools
+
+
+N = int(input())
+total = ['(' for _ in range(N)]
+total.extend([')' for _ in range(N)])
+
+answer = []
+combination = []
+
+for e in set(itertools.permutations(total)):
+    combination.append(''.join(e))
+
+
+def sol(strings):
+    for string in strings:
+        count = 0
+        for character in string:
+            if character == '(':
+                count += 1
+            else:
+                count -= 1
+
+            if count < 0:
+                break
+
+        if count >= 0:
+            answer.append(string)
+
+
+sol(combination)
+print(len(answer), end=' ')
+print(sorted(answer))
+
