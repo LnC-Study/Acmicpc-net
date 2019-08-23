104bc22f8f49e671e1e2bb9bdb7dbea0cfc57967
KwangwonChoi <rhkddnjs99@hotmail.com>
2019-08-24 00:01:00 +0900

    Create ckw.py

---
+++ _Programmers/N으로표현/ckw.py
@@ -0,0 +1,37 @@
+op_list = [
+    lambda a,b : a+ b,
+    lambda a,b : a-b,
+    lambda a,b : a * b,
+    lambda a,b : a / b if b != 0 else None
+]
+
+
+def solution(N, number):
+
+    cache = [set() for _ in range(0, 9)]
+    cache[1].add(N)
+
+    for i in range(2, 9):
+        init_num = int(str(N)*i)
+        cache[i].add(init_num)
+
+        if init_num == number:
+            return i
+
+        for j in range(1,i):
+            for k in range(4):
+                # cache[j]에 있는 모든 값 과 cache[i-j]에 있는 모든 값을 연산해서 넣어줘야 한다.
+                for e1 in cache[j]:
+                    for e2 in cache[i-j]:
+                        unit_ans = op_list[k](e1, e2)
+
+                        if unit_ans is not None:
+                            cache[i].add(unit_ans)
+
+                        if unit_ans == number:
+                            return i
+
+    return -1
+
+
+print(solution(5,12))
